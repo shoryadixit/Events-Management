1716cea702c360833d70665d8a4e782c23b566ca
Shorya Dixit <shauryadt123@gmail.com>
2023-05-21 20:55:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var events = [
+    {
+        name: "Cultural Event",
+        Organized_by: "College",
+        date: "2020-03-01",
+        time: "10:00",
+        location: "College Campus",
+    },
+
+    {
+        name: "Sports Event",
+        Organized_by: "Sports Authority",
+        date: "2020-05-01",
+        time: "10:00",
+        location: "College Campus",
+    },
+
+    {
+        name: "Drama Event",
+        Organized_by: "Dramaics",
+        date: "2021-01-01",
+        time: "10:00",
+        location: "College Campus",
+    },
+
+    {
+        name: "Coding Competition Event",
+        Organized_by: "Coding",
+        date: "2022-01-01",
+        time: "10:00",
+        location: "College Campus",
+    },
+
+    {
+        name: "Lecture Event",
+        Organized_by: "Dramaics",
+        date: "2023-01-01",
+        time: "10:00",
+        location: "College Campus",
+    }
+];
+
